2ad23bc9300b76e7fc304d5d6449f884aa9bca5d
Vishal <vishal198shetty@gmail.com>
2025-01-19 14:16:10 +0530

    added quizes and quizdetails table

--- quiz.sql
+++ quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2025 at 08:08 AM
+-- Generation Time: Jan 19, 2025 at 09:45 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -20,6 +20,48 @@ SET time_zone = "+00:00";
 --
 -- Database: `quiz`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizdetails`
+--
+
+CREATE TABLE `quizdetails` (
+  `quizid` int(5) NOT NULL,
+  `category` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quizdetails`
+--
+
+INSERT INTO `quizdetails` (`quizid`, `category`) VALUES
+(1, 'Science');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `quizes`
+--
+
+CREATE TABLE `quizes` (
+  `id` int(11) NOT NULL,
+  `question` varchar(1000) NOT NULL,
+  `quizid` int(10) NOT NULL,
+  `option1` varchar(222) NOT NULL,
+  `option2` varchar(222) NOT NULL,
+  `option3` varchar(222) NOT NULL,
+  `option4` varchar(222) NOT NULL,
+  `answer` varchar(222) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `quizes`
+--
+
+INSERT INTO `quizes` (`id`, `question`, `quizid`, `option1`, `option2`, `option3`, `option4`, `answer`) VALUES
+(1, '', 1, 'abc', 'asd', 'asd', 'asd', 'abc');
 
 -- --------------------------------------------------------
 
@@ -46,10 +88,43 @@ INSERT INTO `users` (`email`, `name`, `contact`, `password`) VALUES
 --
 
 --
+-- Indexes for table `quizdetails`
+--
+ALTER TABLE `quizdetails`
+  ADD PRIMARY KEY (`quizid`);
+
+--
+-- Indexes for table `quizes`
+--
+ALTER TABLE `quizes`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `quizid` (`quizid`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `quizes`
+--
+ALTER TABLE `quizes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `quizes`
+--
+ALTER TABLE `quizes`
+  ADD CONSTRAINT `quizes_ibfk_1` FOREIGN KEY (`quizid`) REFERENCES `quizdetails` (`quizid`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
