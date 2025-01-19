84d1cbc3fd609bd5f13b69c9a6245fab9c7cd730
Vishal <vishal198shetty@gmail.com>
2025-01-19 18:29:41 +0530

    foreign key and new columns added

--- quiz.sql
+++ quiz.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 19, 2025 at 09:45 AM
+-- Generation Time: Jan 19, 2025 at 01:58 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -29,59 +29,17 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `quizdetails` (
   `quizid` int(5) NOT NULL,
-  `category` varchar(200) NOT NULL
+  `category` varchar(200) NOT NULL,
+  `quizname` varchar(200) NOT NULL,
+  `email` varchar(222) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `quizdetails`
 --
 
-INSERT INTO `quizdetails` (`quizid`, `category`) VALUES
-(1, 'Science');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `quizes`
---
-
-CREATE TABLE `quizes` (
-  `id` int(11) NOT NULL,
-  `question` varchar(1000) NOT NULL,
-  `quizid` int(10) NOT NULL,
-  `option1` varchar(222) NOT NULL,
-  `option2` varchar(222) NOT NULL,
-  `option3` varchar(222) NOT NULL,
-  `option4` varchar(222) NOT NULL,
-  `answer` varchar(222) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `quizes`
---
-
-INSERT INTO `quizes` (`id`, `question`, `quizid`, `option1`, `option2`, `option3`, `option4`, `answer`) VALUES
-(1, '', 1, 'abc', 'asd', 'asd', 'asd', 'abc');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `email` varchar(200) NOT NULL COMMENT 'email works as id',
-  `name` text NOT NULL,
-  `contact` varchar(10) NOT NULL,
-  `password` varchar(200) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`email`, `name`, `contact`, `password`) VALUES
-('vishal198shetty@gmail.com', 'Vishal Shetty', '8088835539', 'Vishal1720');
+INSERT INTO `quizdetails` (`quizid`, `category`, `quizname`, `email`) VALUES
+(12, 'Programming', 'Computer Trivia', 'vishal198shetty@gmail.com');
 
 --
 -- Indexes for dumped tables
@@ -91,40 +49,29 @@ INSERT INTO `users` (`email`, `name`, `contact`, `password`) VALUES
 -- Indexes for table `quizdetails`
 --
 ALTER TABLE `quizdetails`
-  ADD PRIMARY KEY (`quizid`);
-
---
--- Indexes for table `quizes`
---
-ALTER TABLE `quizes`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `quizid` (`quizid`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`email`);
+  ADD PRIMARY KEY (`quizid`),
+  ADD UNIQUE KEY `quizname` (`quizname`),
+  ADD KEY `email` (`email`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `quizes`
+-- AUTO_INCREMENT for table `quizdetails`
 --
-ALTER TABLE `quizes`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `quizdetails`
+  MODIFY `quizid` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `quizes`
+-- Constraints for table `quizdetails`
 --
-ALTER TABLE `quizes`
-  ADD CONSTRAINT `quizes_ibfk_1` FOREIGN KEY (`quizid`) REFERENCES `quizdetails` (`quizid`) ON UPDATE CASCADE;
+ALTER TABLE `quizdetails`
+  ADD CONSTRAINT `quizdetails_ibfk_1` FOREIGN KEY (`email`) REFERENCES `users` (`email`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
